a3b2dbb6289c6315a0ce1bf8eb5885e96a0ed26c
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2021-01-26 20:52:13 +0530

    Major Refactoring

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./api_db.db");
 
+const express = require("express");
+
 function err_cb(err) {
   if (err) console.log(err);
 }
-
-db.run(
-  `CREATE TABLE IF NOT EXISTS api_db (
-    type TEXT NOT NULL,
-    route TEXT NOT NULL,
-    status INTEGER NOT NULL,
-    count INTEGER NOT NULL,
-    PRIMARY KEY(type, route, status)
-  )`,
-  err_cb
-);
 
 const getRoute = (req) => {
   const route = req.route ? req.route.path : ""; // check if the handler exist
@@ -23,59 +14,74 @@ const getRoute = (req) => {
   return route ? `${baseUrl === "/" ? "" : baseUrl}${route}` : "unknown route";
 };
 
-const getDataAsJSON = () => {
-  return new Promise((resolve, reject) => {
-    db.all(`SELECT * FROM api_db`, (err, rows) => {
-      if (err) reject("Error in fetching data from DB", err);
+module.exports = (pass, opt = {}) => {
+  if (!pass || pass.length < 1) throw new Error("Passowrd is required");
 
-      resolve(rows);
-    });
+  let pwd = pass;
+
+  db.serialize(() => {
+    if (opt.freshDB) db.run("DROP TABLE IF EXISTS api_db", err_cb);
+
+    db.run(
+      `CREATE TABLE IF NOT EXISTS api_db (
+      type TEXT NOT NULL,
+      route TEXT NOT NULL,
+      status INTEGER NOT NULL,
+      count INTEGER NOT NULL,
+      PRIMARY KEY(type, route, status)
+    )`,
+      err_cb
+    );
   });
-};
 
-const express = require("express");
-const router = express.Router();
-router.use(express.json());
+  const router = express.Router();
+  router.use(express.json());
 
-router.post("/", async (req, res) => {
-  if (pwd.length < 1) res.status(500).send("Initialization not done properly");
+  const getDataAsJSON = () => {
+    return new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM api_db`, (err, rows) => {
+        if (err) reject("Error in fetching data from DB", err);
 
-  if (req.body.pwd && req.body.pwd === pwd) {
-    const ret = await getDataAsJSON();
-
-    res.json(ret);
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-var pwd = "";
-
-const init = (pass) => {
-  pwd = pass;
-};
-
-const middleware = (req, res, next) => {
-  res.on("finish", () => {
-    console.log(`${req.method} ${getRoute(req)} ${res.statusCode}`);
-
-    db.serialize(() => {
-      db.run(
-        `INSERT INTO api_db VALUES ($type, $route, $status, 1) ON CONFLICT(type, route, status)
-          DO UPDATE SET count = count + 1`,
-        { $type: req.method, $route: getRoute(req), $status: res.statusCode },
-        err_cb
-      );
-
-      db.each("SELECT * FROM api_db", (err, row) => {
-        if (err) console.log(err);
-
-        console.log(row);
+        resolve(rows);
       });
     });
+  };
+
+  router.post("/", async (req, res) => {
+    if (pwd.length < 1)
+      res.status(500).send("Initialization not done properly");
+
+    if (req.body.pwd && req.body.pwd === pwd) {
+      const ret = await getDataAsJSON();
+
+      res.json(ret);
+    } else {
+      res.sendStatus(403);
+    }
   });
 
-  next();
-};
+  const middleware = (req, res, next) => {
+    res.on("finish", () => {
+      console.log(`${req.method} ${getRoute(req)} ${res.statusCode}`);
 
-module.exports = { init, middleware, router };
+      db.serialize(() => {
+        db.run(
+          `INSERT INTO api_db VALUES ($type, $route, $status, 1) ON CONFLICT(type, route, status)
+            DO UPDATE SET count = count + 1`,
+          { $type: req.method, $route: getRoute(req), $status: res.statusCode },
+          err_cb
+        );
+
+        db.each("SELECT * FROM api_db", (err, row) => {
+          if (err) console.log(err);
+
+          console.log(row);
+        });
+      });
+    });
+
+    next();
+  };
+
+  return { middleware, router, getDataAsJSON };
+};
--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const stats = require("../index");
+const stats = require("../index")("password");
 
 const app = express();
 
-stats.init("password");
 app.use("/stats", stats.router);
 app.use(stats.middleware);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
-})
+});
 
 app.listen(5000, () => {
   console.log("Listening");
